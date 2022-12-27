b3481324b9a267b3cd50db3a26ec8a70a6790750
G0od1k <g0cd1k@gmail.com>
2022-12-27 21:32:43 +0200

    Replaced tabs with spaces

--- fun.js
+++ fun.js
@@ -2,83 +2,83 @@ const fs = require("fs")
 const path = require("path")
 
 Object.defineProperty(Array.prototype, "chunk", {
-	value: function (chunkSize) {
-		var temporal = []
+    value: function (chunkSize) {
+        var temporal = []
 
-		for (var i = 0; i < this.length; i += chunkSize) {
-			temporal.push(this.slice(i, i + chunkSize))
-		}
+        for (var i = 0; i < this.length; i += chunkSize) {
+            temporal.push(this.slice(i, i + chunkSize))
+        }
 
-		return temporal
-	},
+        return temporal
+    },
 })
 
 function log(bot, user, msg) {
-	console.log(
-		`\x1b[36m[${bot.name}]\x1b[0m \x1b[32m[${user.name}]\x1b[0m ${msg}`
-	)
+    console.log(
+        `\x1b[36m[${bot.name}]\x1b[0m \x1b[32m[${user.name}]\x1b[0m ${msg}`
+    )
 }
 
 function formatFileSize(number = 0) {
-	return Intl.NumberFormat(`en`, {
-		notation: `compact`,
-		style: `unit`,
-		unit: `byte`,
-		unitDisplay: `narrow`,
-	}).format(number)
+    return Intl.NumberFormat(`en`, {
+        notation: `compact`,
+        style: `unit`,
+        unit: `byte`,
+        unitDisplay: `narrow`,
+    }).format(number)
 }
 
 function verify(msg, bot, cmd) {
-	if (bot.cfg.users[msg.chat.id] === undefined) {
-		bot.sendMessage(msg.chat.id, bot.cfg.msg.accessDenied)
-		log(bot.cfg, { name: msg.chat.id }, bot.cfg.msg.accessDenied)
-		return true
-	}
-	if (cmd) {
-		if (!bot.cfg.users[msg.chat.id][cmd]) {
-			bot.sendMessage(msg.chat.id, bot.cfg.msg.accessDenied)
-			log(
-				bot.cfg,
-				bot.cfg.users[msg.chat.id],
-				`\x1b[33m[${cmd}]\x1b[0m ` + bot.cfg.msg.accessDenied
-			)
-			return true
-		}
-	}
-	return false
+    if (bot.cfg.users[msg.chat.id] === undefined) {
+        bot.sendMessage(msg.chat.id, bot.cfg.msg.accessDenied)
+        log(bot.cfg, { name: msg.chat.id }, bot.cfg.msg.accessDenied)
+        return true
+    }
+    if (cmd) {
+        if (!bot.cfg.users[msg.chat.id][cmd]) {
+            bot.sendMessage(msg.chat.id, bot.cfg.msg.accessDenied)
+            log(
+                bot.cfg,
+                bot.cfg.users[msg.chat.id],
+                `\x1b[33m[${cmd}]\x1b[0m ` + bot.cfg.msg.accessDenied
+            )
+            return true
+        }
+    }
+    return false
 }
 
 function icon(pth, cfg) {
-	const stat = fs.statSync(pth)
+    const stat = fs.statSync(pth)
 
-	let icon = cfg.defaultFileIcon
+    let icon = cfg.defaultFileIcon
 
-	for (i of Object.keys(cfg.icons)) {
-		if (RegExp(cfg.icons[i], `i`).test(path.parse(pth).ext)) {
-			icon = i
-			continue
-		}
-	}
+    for (i of Object.keys(cfg.icons)) {
+        if (RegExp(cfg.icons[i], `i`).test(path.parse(pth).ext)) {
+            icon = i
+            continue
+        }
+    }
 
-	return stat.isDirectory() ? cfg.defaultDirIcon : icon
+    return stat.isDirectory() ? cfg.defaultDirIcon : icon
 }
 
 function resolve(user, msg, cfg) {
-	user.path ??= cfg.home
+    user.path ??= cfg.home
 
-	let acc = true
+    let acc = true
 
-	cfg.excludePath.concat(user.excludePath ?? []).forEach((x) => {
-		acc &&= !new RegExp(x, "i").test(path.resolve(user.path, msg.text))
-	})
+    cfg.excludePath.concat(user.excludePath ?? []).forEach((x) => {
+        acc &&= !new RegExp(x, "i").test(path.resolve(user.path, msg.text))
+    })
 
-	user.includePath?.forEach((x) => {
-		acc ||= new RegExp(x, "i").test(path.resolve(user.path, msg.text))
-	})
+    user.includePath?.forEach((x) => {
+        acc ||= new RegExp(x, "i").test(path.resolve(user.path, msg.text))
+    })
 
-	user.path = acc ? path.resolve(user.path, msg.text) : user.path
+    user.path = acc ? path.resolve(user.path, msg.text) : user.path
 
-	log(cfg, user, user.path)
+    log(cfg, user, user.path)
 }
 
 module.exports = { log, formatFileSize, verify, icon, resolve }
--- index.js
+++ index.js
@@ -8,149 +8,149 @@ process.env["NTBA_FIX_350"] = 1
 process.title = "tg-fs"
 
 require("./config.json").forEach((cfg) => {
-	const bot = new TelegramBot(cfg.token, {
-		polling: true,
-		filepath: false,
-	})
+    const bot = new TelegramBot(cfg.token, {
+        polling: true,
+        filepath: false,
+    })
 
-	bot.cfg = cfg
+    bot.cfg = cfg
 
-	const users = cfg.users
+    const users = cfg.users
 
-	//? ====================[With Permission]====================
+    //? ====================[With Permission]====================
 
-	bot.onText(/\/download/, (msg) => {
-		if (verify(msg, bot, "download")) return 0
+    bot.onText(/\/download/, (msg) => {
+        if (verify(msg, bot, "download")) return 0
 
-		const stat = fs.statSync(users[msg.chat.id].path)
-		if (stat.size) {
-			bot.sendDocument(
-				msg.chat.id,
-				fs.createReadStream(users[msg.chat.id].path)
-			)
-		} else {
-			bot.sendMessage(msg.chat.id, cfg.msg.emptyFile)
-		}
-		log(cfg, users[msg.chat.id], `download`)
-	})
+        const stat = fs.statSync(users[msg.chat.id].path)
+        if (stat.size) {
+            bot.sendDocument(
+                msg.chat.id,
+                fs.createReadStream(users[msg.chat.id].path)
+            )
+        } else {
+            bot.sendMessage(msg.chat.id, cfg.msg.emptyFile)
+        }
+        log(cfg, users[msg.chat.id], `download`)
+    })
 
-	bot.onText(/\/cat/, (msg) => {
-		if (verify(msg, bot, "cat")) return 0
+    bot.onText(/\/cat/, (msg) => {
+        if (verify(msg, bot, "cat")) return 0
 
-		const stat = fs.statSync(users[msg.chat.id].path)
-		const stream = new fs.ReadStream(users[msg.chat.id].path)
+        const stat = fs.statSync(users[msg.chat.id].path)
+        const stream = new fs.ReadStream(users[msg.chat.id].path)
 
-		stream.on("readable", function () {
-			const data = stream.read(Math.min(4096, stat.size))
-			stream.close()
+        stream.on("readable", function () {
+            const data = stream.read(Math.min(4096, stat.size))
+            stream.close()
 
-			const text = new TextDecoder()
-				.decode(data ?? new Buffer.from(cfg.msg.emptyFile))
-				.replace(/&/g, `&amp;`)
-				.replace(/</g, `&lt;`)
-				.replace(/>/g, `&gt;`)
+            const text = new TextDecoder()
+                .decode(data ?? new Buffer.from(cfg.msg.emptyFile))
+                .replace(/&/g, `&amp;`)
+                .replace(/</g, `&lt;`)
+                .replace(/>/g, `&gt;`)
 
-			bot.sendMessage(msg.chat.id, `<pre>${text}</pre>`, {
-				parse_mode: "HTML",
-			})
-			log(cfg, users[msg.chat.id], `cat`)
-		})
-	})
+            bot.sendMessage(msg.chat.id, `<pre>${text}</pre>`, {
+                parse_mode: "HTML",
+            })
+            log(cfg, users[msg.chat.id], `cat`)
+        })
+    })
 
-	//? ====================[Without Permission]====================
+    //? ====================[Without Permission]====================
 
-	bot.onText(/\/pwd/, (msg) => {
-		if (verify(msg, bot)) return 0
+    bot.onText(/\/pwd/, (msg) => {
+        if (verify(msg, bot)) return 0
 
-		bot.sendMessage(msg.chat.id, users[msg.chat.id].path)
-		log(cfg, users[msg.chat.id], `pwd`)
-	})
+        bot.sendMessage(msg.chat.id, users[msg.chat.id].path)
+        log(cfg, users[msg.chat.id], `pwd`)
+    })
 
-	bot.onText(/\/home/, (msg) => {
-		users[msg.chat.id].path = cfg.home
-		msg.text = `.`
-		log(cfg, users[msg.chat.id], `home`)
-		bot.emit(`message`, msg)
-	})
+    bot.onText(/\/home/, (msg) => {
+        users[msg.chat.id].path = cfg.home
+        msg.text = `.`
+        log(cfg, users[msg.chat.id], `home`)
+        bot.emit(`message`, msg)
+    })
 
-	bot.onText(/\/root/, (msg) => {
-		if (verify(msg, bot)) return 0
+    bot.onText(/\/root/, (msg) => {
+        if (verify(msg, bot)) return 0
 
-		let result = ``
-		let tpath = users[msg.chat.id].path
+        let result = ``
+        let tpath = users[msg.chat.id].path
 
-		while (tpath != path.parse(tpath).dir) {
-			tpath = path.parse(tpath).dir
-			result = `\`${tpath}\`\n` + result
-		}
+        while (tpath != path.parse(tpath).dir) {
+            tpath = path.parse(tpath).dir
+            result = `\`${tpath}\`\n` + result
+        }
 
-		bot.sendMessage(msg.chat.id, result, { parse_mode: `Markdown` })
-		log(cfg, users[msg.chat.id], `root`)
-	})
+        bot.sendMessage(msg.chat.id, result, { parse_mode: `Markdown` })
+        log(cfg, users[msg.chat.id], `root`)
+    })
 
-	bot.onText(/^(?<!\/)([^\/\n].*)/, (msg) => {
-		if (verify(msg, bot)) return 0
+    bot.onText(/^(?<!\/)([^\/\n].*)/, (msg) => {
+        if (verify(msg, bot)) return 0
 
-		const user = users[msg.chat.id]
+        const user = users[msg.chat.id]
 
-		resolve(user, msg, cfg)
+        resolve(user, msg, cfg)
 
-		let stat = fs.statSync(user.path)
+        let stat = fs.statSync(user.path)
 
-		if (stat.isDirectory()) {
-			;[".."]
-				.concat(fs.readdirSync(user.path))
-				.chunk(30)
-				.forEach((x, i, z) => {
-					bot.sendMessage(
-						msg.chat.id,
-						`\`${user.path}\`\n\`=[${`${i + 1}/${z.length}]`.padEnd(
-							34,
-							"="
-						)}\`\n` +
-							x
-								.map((x) => {
-									const stat = fs.statSync(
-										path.resolve(user.path, x)
-									)
+        if (stat.isDirectory()) {
+            ;[".."]
+                .concat(fs.readdirSync(user.path))
+                .chunk(30)
+                .forEach((x, i, z) => {
+                    bot.sendMessage(
+                        msg.chat.id,
+                        `\`${user.path}\`\n\`=[${`${i + 1}/${z.length}]`.padEnd(
+                            34,
+                            "="
+                        )}\`\n` +
+                            x
+                                .map((x) => {
+                                    const stat = fs.statSync(
+                                        path.resolve(user.path, x)
+                                    )
 
-									return (
-										icon(path.resolve(user.path, x), cfg) +
-										`\`${x}${
-											stat.isDirectory() ? `/` : ``
-										}\``
-									)
-								})
-								.join(`\n`),
-						{
-							parse_mode: "Markdown",
-							reply_markup: {
-								remove_keyboard: true,
-							},
-						}
-					)
-				})
-		} else {
-			bot.sendMessage(
-				msg.chat.id,
-				`=== *${path.basename(
-					user.path
-				)}* ===\n\nSize: ${formatFileSize(stat.size)}\nPath: \`${
-					user.path
-				}\``,
-				{
-					parse_mode: "Markdown",
-					reply_markup: {
-						keyboard: [
-							["../"],
-							...["download", "cat"].map((x) => [
-								user[x] ? `/` + x : ``,
-							]),
-						],
-						resize_keyboard: true,
-					},
-				}
-			)
-		}
-	})
+                                    return (
+                                        icon(path.resolve(user.path, x), cfg) +
+                                        `\`${x}${
+                                            stat.isDirectory() ? `/` : ``
+                                        }\``
+                                    )
+                                })
+                                .join(`\n`),
+                        {
+                            parse_mode: "Markdown",
+                            reply_markup: {
+                                remove_keyboard: true,
+                            },
+                        }
+                    )
+                })
+        } else {
+            bot.sendMessage(
+                msg.chat.id,
+                `=== *${path.basename(
+                    user.path
+                )}* ===\n\nSize: ${formatFileSize(stat.size)}\nPath: \`${
+                    user.path
+                }\``,
+                {
+                    parse_mode: "Markdown",
+                    reply_markup: {
+                        keyboard: [
+                            ["../"],
+                            ...["download", "cat"].map((x) => [
+                                user[x] ? `/` + x : ``,
+                            ]),
+                        ],
+                        resize_keyboard: true,
+                    },
+                }
+            )
+        }
+    })
 })
